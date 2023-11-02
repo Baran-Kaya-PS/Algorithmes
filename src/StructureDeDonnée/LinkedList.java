8a4db971a909e97057b7b6775fc8c7373a92ff65
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2023-11-02 16:41:09 +0100

    LinkedList implémentation

--- src/StructureDeDonnée/LinkedList.java
+++ src/StructureDeDonnée/LinkedList.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 
 public class LinkedList<T> implements Iterable<T>{
     Node<T> head;
+    Node<T> tail;
 
     public LinkedList(){
         this.head = null;
@@ -15,33 +16,182 @@ public class LinkedList<T> implements Iterable<T>{
         }
     }
     public void add(T data){
+        Node<T> newNode = new Node<>(data);
         if (head == null){
-            head = new Node<>(data);
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+    }
+    public void add(T data, int index){
+        checkIndex(index);
+        if (index == 0){
+            Node<T> node = new Node<>(data);
+            node.setNext(head);
+            node = head;
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++){
+            current = current.getNext();
+        }
+        Node<T>old_next = current.getNext();
+        current.setNext(new Node<>(data));
+        current.getNext().setNext(old_next);
+    }
+    public void clear(){
+        head = null;
+        tail = null;
+    }
+
+    public boolean contains(T value){
+        Node<T> curr = head;
+        while (curr != null){
+            if (curr.get().equals(value)){
+                return true;
+            }
+            curr = curr.getNext();
+        }
+        return false;
+    }
+
+    public void removeFirstValue(T value){
+        if (head == null) return;
+        if (head.get().equals(value)){
+            head = head.getNext();
+            if (head == null) tail = null;
             return;
         }
-        Node<T> current = head;
-        while (current.getNext() != null){
-            current = current.getNext();
+        Node<T> curr = head;
+        while (curr.getNext() != null){
+            if (curr.getNext().get().equals(value)){
+                curr.setNext(curr.getNext().getNext());
+                if (curr.getNext() == null) tail = curr; // Si on supprime le dernier élément
+                return;
+            }
+            curr = curr.getNext();
         }
-        current.setNext(new Node<T>(data));
     }
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder(); // sb more fast
-        sb.append("[");
-        Node<T> current = head;
-        while (current != null) {
-            sb.append(current.get());
-            current = current.getNext();
-            if (current != null) {
-                sb.append(", ");
+
+    public void removeAll(T value){
+        while (head != null && head.get().equals(value)) {
+            head = head.getNext();
+        }
+
+        if(head == null) {
+            tail = null;
+            return;
+        }
+
+        Node<T> curr = head;
+        while (curr.getNext() != null) {
+            if (curr.getNext().get().equals(value)) {
+                curr.setNext(curr.getNext().getNext());
+                if(curr.getNext() == null) tail = curr; // Si on supprime le dernier élément
+            } else {
+                curr = curr.getNext();
             }
         }
-        sb.append("]");
-        return sb.toString();
+    }
+    public boolean hasCycle(){
+        Node<T> slow = head;
+        Node<T> fast = head.getNext(); // pour eviter de return true dès le début
+        while (fast != null && fast.getNext() != null) {
+            if (slow.equals(fast)) return true;
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+        }
+        return false;
+    }
+    public LinkedList<T> reverseList(){
+        //TODO
+        return null;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof LinkedList)) return false;
+        LinkedList<?> other = (LinkedList<?>) obj;
+        Node<T> currentA = this.head;
+        Node<?> currentB = other.head;
+
+        while (currentA != null && currentB != null) {
+            if (!currentA.get().equals(currentB.get())) {
+                return false;
+            }
+            currentA = currentA.getNext();
+            currentB = currentB.getNext();
+        }
+        return currentA == null && currentB == null;
     }
 
+    public T get(int index){
+        checkIndex(index);
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        return current.get();
+    }
 
+    public int indexOf(T data){
+        int index = 0;
+        Node<T> current = head;
+        while (current != null) {
+            if (current.get().equals(data)) {
+                return index;
+            }
+            index++;
+            current = current.getNext();
+        }
+        return -1;
+    }
+
+    public void set(int index, T data){
+        checkIndex(index);
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        current.set(data);
+    }
+
+    public void remove(int index){
+        checkIndex(index);
+        if (index == 0){
+            head = head.getNext();
+            if (head == null) tail = null;
+            return;
+        }
+
+        Node<T> current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.getNext();
+        }
+
+        current.setNext(current.getNext().getNext());
+        if (current.getNext() == null) tail = current;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private int size() {
+        int size = 0;
+        Node<T> current = head.getNext();
+        while (current != null){
+            size++;
+            current = current.getNext();
+        }
+        return size;
+    }
+    public static void main(String[] args) {
+
+    }
     @Override
     public Iterator<T> iterator() {
         return new LinkedListIterator();
--- src/StructureDeDonnée/Node.java
+++ src/StructureDeDonnée/Node.java
@@ -20,4 +20,29 @@ public class Node<T> {
     public Node<T> getNext(){
         return this.next;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Vérifie si les références sont identiques
+        if (this == obj) {
+            return true;
+        }
+        // Vérifie si l'objet passé est null ou s'il est d'une classe différente
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        // Cast l'objet vers Node<T>
+        Node<?> other = (Node<?>) obj;
+        // Compare les valeurs, en gérant le cas où elles sont null
+        if (val == null) {
+            return other.val == null;
+        } else {
+            return val.equals(other.val);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return val != null ? val.hashCode() : 0;
+    }
 }
